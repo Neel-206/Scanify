d19aa16138bf3660d64e9b3a01e5286574529880
Neel-206 <nkgediya206@gmail.com>
2026-05-14 17:34:11 +0530

    feat: implement sign-up, login, and forgot password screens with associated UI resources and layouts

--- app/src/main/java/com/example/scanify/forgot_screen.java
+++ app/src/main/java/com/example/scanify/forgot_screen.java
@@ -1,8 +1,11 @@
 package com.example.scanify;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class forgot_screen extends AppCompatActivity {
 
+    Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        back = findViewById(R.id.btnBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Closes current activity and returns to previous one
+            }
         });
+
     }
 }
--- app/src/main/java/com/example/scanify/login_screen.java
+++ app/src/main/java/com/example/scanify/login_screen.java
@@ -1,16 +1,41 @@
 package com.example.scanify;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login_screen extends AppCompatActivity {
 
+    Button forgot,signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_screen);
+
+        forgot =  findViewById(R.id.btnForgot);
+        signup = findViewById(R.id.btnSignUp);
+
+        forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_screen.this, forgot_screen.class);
+                startActivity(intent);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_screen.this, signUp_screen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
